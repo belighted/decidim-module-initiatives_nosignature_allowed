181a09e8e897329302be6010c209219b701cf61a
Andrés Cirugeda <andres@aspgems.com>
2020-04-27 17:18:13 +0200

    Apply changes from custom signature end date PR on decidim

--- app/forms/decidim/initiatives/initiative_form.rb
+++ app/forms/decidim/initiatives/initiative_form.rb
@@ -14,6 +14,7 @@ module Decidim
       attribute :scope_id, Integer
       attribute :decidim_user_group_id, Integer
       attribute :signature_type, String
+      attribute :signature_end_date, Date
       attribute :state, String
       attribute :no_signature, Boolean
 
@@ -22,6 +23,9 @@ module Decidim
       validates :signature_type, presence: true
       validates :type_id, presence: true
       validate :scope_exists
+      validates :signature_end_date, date: { after: Date.current }, if: lambda { |form|
+        type.custom_signature_end_date_enabled? && form.signature_end_date.present?
+      }
       validate :check_no_signature
 
       def map_model(model)
